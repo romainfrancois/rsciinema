88f1428b25ed41ccea59b588961948f6314e598a
Romain Francois <romain@thinkr.fr>
2017-10-06 21:53:20 +0200

    some functions to extract information from rstudio themes

--- data-raw/themes.R
+++ data-raw/themes.R
@@ -10,17 +10,27 @@ raw_directory <- "https://raw.githubusercontent.com/rstudio/rstudio/master/src/g
 rstudio_theme_names <- read_html(directory) %>%
   html_nodes("a[title$='css']") %>%
   html_text() %>%
-  str_replace( "[.]css$", "")
+  str_replace( "[.]css$", "") %>%
+  set_names(.)
 
-rstudio_themes <- map_df( rstudio_theme_names, ~{
-  theme <- .
-  read_css(glue("{raw_directory}/{theme}.css")) %>%
-    mutate( theme = theme )
+rstudio_themes <- map( rstudio_theme_names, ~{
+  read_css(glue("{raw_directory}/{.}.css"))
 })
 
-rstudio_themes <- mutate( rstudio_themes,
-  dark =
-  )
-
-
 use_data( rstudio_themes, overwrite = TRUE)
+
+ascii_css <- read_css("inst/htmlwidgets/lib/asciinema-player-2.5.0/asciinema-player.css")
+
+default_fg_color <- function(rs_theme){
+  filter(rs_theme, str_detect(rule, "^.ace_editor"), property == "color" ) %>% pull(value)
+}
+default_bg_color <- function(rs_theme){
+  filter(rs_theme, str_detect(rule, "^.ace_editor"), property == "color" ) %>% pull(value)
+}
+
+kw <- function(rs_theme){
+  filter(rs_theme, str_detect(rule, "^.ace_keyword") )
+}
+map_df( rstudio_themes, kw, .id = "themes") %>% select(-property)
+
+
